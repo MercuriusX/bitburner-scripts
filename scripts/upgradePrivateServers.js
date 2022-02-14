3c5c4db1d3c38cb050cb8023243a2c3bf520a81b
MercuriusX <75004966+MercuriusX@users.noreply.github.com>
2022-02-14 13:07:11 -0500

    checks for money to buy all 25 servers, not just one, added total cost to failure output

--- scripts/upgradePrivateServers.js
+++ scripts/upgradePrivateServers.js
@@ -3,8 +3,10 @@ export async function main(ns) {
 	let multiplier = ns.args[0]; if (ns.args[0] == null) { multiplier = 1; }
 	let ram = ns.getServerMaxRam("pserv-0");
 	let newRam = ram * 2 * multiplier;
+	let upgradeAvailable = ns.getServerMoneyAvailable("home") > newRam * 55000 * 25;
+	let totalUpgradeCost = newRam * 55000 * 25
 
-	if (ns.getServerMoneyAvailable("home") > newRam * 55000) {
+	if (upgradeAvailable) {
 		for (let i = 0; i < ns.getPurchasedServerLimit(); i++) {
 			let hostname = "pserv-" + i;
 			ns.tprint("Upgrading: " + hostname);
@@ -13,6 +15,6 @@ export async function main(ns) {
 			ns.purchaseServer(hostname, newRam);
 		}
 	} else {
-		ns.tprint("Not enough money to upgrade all pserv ram");
+		ns.tprint("Not enough money to upgrade all pserv ram: $" + totalUpgradeCost.toLocaleString());
 	}
 }
