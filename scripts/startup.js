4bad18da39ee5bc63909d350355ddf29a3b49f17
MercuriusX <75004966+MercuriusX@users.noreply.github.com>
2022-02-14 15:17:55 -0500

    renamed initial ram variable, added conditional swapping of target list

--- scripts/startup.js
+++ scripts/startup.js
@@ -7,9 +7,13 @@ export async function main(ns) {
 	*/
 	let homeScan = ns.scan("home");
 	let privateServerList = [];
+	let initialPrivateServerRam = 1024;
 	let currentTargets = ["hong-fang-tea", "nectar-net", "neo-net", "zer0", "harakiri-sushi", "netlink", "aevum-police", "summit-uni", "millenium-fitness", "catalyst", "omega-net", "rothman-uni", "rho-construction", "silver-helix", "max-hardware", "phantasy", "the-hub", "alpha-ent", "infocomm", "unitalife", "snap-fitness", "comptek", "syscore", "johnson-ortho", "zb-institute"];
-	let privateServerRam = 2048;
 
+	if (ns.getHackingLevel() > 3000) {
+		currentTargets = ["nova-med", "aerocorp", "stormtech", "unitalife", "zb-def", "icarus", "defcomm", "omnia", "powerhouse-fitness", "zb-institute", "titan-labs", "applied-energetics", "galactic-cyber", "infocomm", "solaris", "taiyang-digital", "vitalife", "lexo-corp", "helios", "alpha-ent", "rho-construction", "microdyne", "snap-fitness", "syscore", "catalyst"]
+	}
+	
 	if (ns.getHackingLevel() < 1000) {
 		if (ns.hasRootAccess("foodnstuff") == false) { ns.nuke("foodnstuff"); }
 		ns.run("hack.js", Math.floor(ns.getServerMaxRam("home") / ns.getScriptRam("hack.js") - 30), "foodnstuff");
@@ -48,13 +52,13 @@ export async function main(ns) {
 	if (!(ns.isRunning("batchHack.js", "home", "joesguns", "home"))) {
 		ns.run("distributeScript.js", 1, "joesguns", "home");
 	}
-	while (ns.getServerMoneyAvailable("home") < (privateServerRam * 55000 * 26) && ns.getPurchasedServers().length != 25) {
+	while (ns.getServerMoneyAvailable("home") < (initialPrivateServerRam * 55000 * 26) && ns.getPurchasedServers().length != 25) {
 		await ns.sleep(500);
 	}
 	for (let i = 0; i < ns.getPurchasedServerLimit(); i++) {
 		let pservHostname = "pserv-" + i;
 		if (homeScan.indexOf(pservHostname) == -1) {
-			ns.purchaseServer(pservHostname, privateServerRam);
+			ns.purchaseServer(pservHostname, initialPrivateServerRam);
 		}
 		if (privateServerList.indexOf(pservHostname) == -1) {
 			privateServerList.push(pservHostname);
